cbbe1592d619e4228c383bffe87ff37ce22efc97
snssuresh877 <snssuresh877@users.noreply.github.com>
2026-09-18 17:36:14 +0530

    feat: Add interactive left/right navigation arrows to workspace indicator

--- src/shell/bar/workspace_widget.cpp
+++ src/shell/bar/workspace_widget.cpp
@@ -10,22 +10,27 @@ std::vector<GtkWidget*> WorkspaceWidget::buttons;
 int WorkspaceWidget::current_active = 1;
 
 GtkWidget* WorkspaceWidget::create(int count) {
-    GtkWidget* box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6);
+    GtkWidget* box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
     gtk_widget_add_css_class(box, "workspace-container");
 
     buttons.clear();
 
+    // Left Arrow
+    GtkWidget* btn_prev = gtk_button_new();
+    gtk_widget_add_css_class(btn_prev, "workspace-btn");
+    GtkWidget* lbl_prev = gtk_label_new("❮");
+    gtk_container_add(GTK_CONTAINER(btn_prev), lbl_prev);
+    g_signal_connect(btn_prev, "clicked", G_CALLBACK(+[](GtkButton*, gpointer) {
+        HyprlandIPC::switch_workspace_relative(-1);
+    }), nullptr);
+    gtk_box_pack_start(GTK_BOX(box), btn_prev, FALSE, FALSE, 0);
+
+    // Current Number
     GtkWidget* btn = gtk_button_new();
     gtk_widget_add_css_class(btn, "workspace-btn");
     gtk_widget_add_css_class(btn, "active"); // Always active style
-
     GtkWidget* lbl = gtk_label_new("1");
     gtk_container_add(GTK_CONTAINER(btn), lbl);
-
-    g_signal_connect(btn, "clicked", G_CALLBACK(+[](GtkButton*, gpointer) {
-        HyprlandIPC::switch_workspace_relative(1);
-    }), nullptr);
-
     gtk_widget_add_events(btn, GDK_SCROLL_MASK);
     g_signal_connect(btn, "scroll-event", G_CALLBACK(+[](GtkWidget*, GdkEventScroll* event, gpointer) -> gboolean {
         if (event->direction == GDK_SCROLL_UP || event->delta_y < 0) {
@@ -37,9 +42,18 @@ GtkWidget* WorkspaceWidget::create(int count) {
         }
         return FALSE;
     }), nullptr);
-
     gtk_box_pack_start(GTK_BOX(box), btn, FALSE, FALSE, 0);
     buttons.push_back(btn);
+
+    // Right Arrow
+    GtkWidget* btn_next = gtk_button_new();
+    gtk_widget_add_css_class(btn_next, "workspace-btn");
+    GtkWidget* lbl_next = gtk_label_new("❯");
+    gtk_container_add(GTK_CONTAINER(btn_next), lbl_next);
+    g_signal_connect(btn_next, "clicked", G_CALLBACK(+[](GtkButton*, gpointer) {
+        HyprlandIPC::switch_workspace_relative(1);
+    }), nullptr);
+    gtk_box_pack_start(GTK_BOX(box), btn_next, FALSE, FALSE, 0);
 
     HyprlandIPC::instance().set_workspace_callback([](int active_id) {
         update_active(active_id);
